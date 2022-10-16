003ed5d28d0fb8c5ceab7ead049bde2a22a736b8
Jacob Weisz <inbox@jacobweisz.com>
2022-10-16 02:19:21 -0500

    Send a heartbeat to sync server

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace HAConfig;
 using SQLite;
+using System.Net.Http;
 
 public partial class MainPage : ContentPage
 {
@@ -11,12 +12,22 @@ public partial class MainPage : ContentPage
 		OpenConfigTable();
 	}
 
-	private void SaveBtn_Clicked(object sender, EventArgs e)
+	private async void SaveBtn_Clicked(object sender, EventArgs e)
 	{
 		AddOrUpdateConfig(new Config { Key = "Sync_ServerURL", Value = Sync_ServerURL.Text });
 		AddOrUpdateConfig(new Config { Key = "Sync_SandstormToken", Value = Sync_SandstormToken.Text });
 		AddOrUpdateConfig(new Config { Key = "Sync_AccessKey", Value = Sync_AccessKey.Text });
+
+		// The config tool will send a single heartbeat to confirm the Sandstorm offer template
+		HttpClient heartbeatClient = new();
+		HttpRequestMessage heartbeatMessage = new(HttpMethod.Post, Sync_ServerURL.Text + "?message_type=heartbeat&destination=server&access_key=" + Sync_AccessKey.Text + "&message=none&user_agent=XRFAgentConfig/1.0.0");
+		string EncodedCreds = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes("sandstorm:" + Sync_SandstormToken.Text));
+		heartbeatMessage.Headers.Add("Authorization", "Basic " + EncodedCreds);
+		HttpResponseMessage heartbeatResponse = await heartbeatClient.SendAsync(heartbeatMessage);
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 		DisplayAlert("Saved", "Configuration values have been saved", "OK");
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
 	}
 
